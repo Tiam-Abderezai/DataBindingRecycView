49d77fd0b8e14ec06b2c2cb06fbee3e5fa57df60
Tiam Abderezai <tiam.abderezai@gmail.com>
2020-04-29 22:21:45 -0700

    RecyclerView and Adapter features were added. Credit: Prabeesh R.K

--- app/src/main/java/com/example/databindingjava/MainActivity.java
+++ app/src/main/java/com/example/databindingjava/MainActivity.java
@@ -2,20 +2,46 @@ package com.example.databindingjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.Bundle;
 
 import com.example.databindingjava.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    private UserAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
 
         User user = new User("Sabine Becker", "s.becker@email.com");
 
-        binding.setUser(user);
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        binding.recyclerView.setHasFixedSize(true);
+
+        adapter = new UserAdapter(this, prepareUser());
+        binding.recyclerView.setAdapter(adapter);
     }
+
+    private List<User> prepareUser() {
+        List<String> names = Arrays.asList(getResources().getStringArray(R.array.names));
+        List<String> emails = Arrays.asList(getResources().getStringArray(R.array.emails));
+        List<User> Names = new ArrayList<>();
+        int count = 0;
+
+        for(String name : names) {
+            Names.add(new User (name, emails.get(count)));
+            count++;
+        }
+        return Names;
+     }
+
 }
